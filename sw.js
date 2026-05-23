dafec3155dd099bd81f282baa6b74a3142186da4
taichichi-lab <yuugen35@gmail.com>
2026-05-23 10:04:39 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+// フリマ電卓 Service Worker（オフライン対応）
+const CACHE_NAME = 'furima-keisanki-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/apple-touch-icon.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
